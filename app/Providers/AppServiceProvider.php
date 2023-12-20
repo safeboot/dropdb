e54f3dca043fd51407c5b5d51dd456d29724c99f
Sanjin Brajević <mynamesafeboot@gmail.com>
2023-12-20 11:26:09 +0100

    Fixed IPs getting obfuscated through CF

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Symfony\Component\HttpFoundation\Request;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -9,16 +10,18 @@ class AppServiceProvider extends ServiceProvider
     /**
      * Register any application services.
      */
-    public function register(): void
-    {
+    public function register(): void {
+
         //
+
     }
 
     /**
      * Bootstrap any application services.
      */
-    public function boot(): void
-    {
-        //
+    public function boot(): void {
+
+        Request::setTrustedProxies(['REMOTE_ADDR'],  Request::HEADER_X_FORWARDED_FOR);
+
     }
 }
